72cd375cbba6331c06406eb97f09736f1b2d5421
masterng <nasterng@yansex.ru>
2023-10-23 16:46:06 +0500

    закончил планшетную версию

--- js/main.js
+++ js/main.js
@@ -1,11 +1,40 @@
-// аккордион
 
 window.addEventListener('load', function () {
 
+    // меню бургер
+
+    const menuBurger = document.querySelector('.btn_menu');
+    const mobileClouse = document.querySelector('.momile_clouse');
+    const mobileList = document.querySelector('.mobile_list');
+    const navMobile = document.querySelector('.nav_mobile');
+
+    menuBurger.addEventListener('click', function () {
+
+        if (navMobile.classList.contains('display_none')) {
+            navMobile.classList.remove('display_none');
+        }
+
+    });
+
+    mobileClouse.addEventListener('click', function () {
+
+        navMobile.classList.add('display_none');
+
+    });
+
+    mobileList.addEventListener('click', function () {
+
+        navMobile.classList.add('display_none');
+
+    });
+
+
+    // аккордион
     const accordion = document.querySelectorAll('.accordion');
 
     for (item of accordion) {
         item.addEventListener('click', function () {
+
             if (this.classList.contains('active')) {
                 this.classList.remove('active');
             } else {
@@ -33,4 +62,5 @@ window.addEventListener('load', function () {
 
         });
     });
+
 });
